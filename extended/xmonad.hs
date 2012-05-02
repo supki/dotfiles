853ff8111bcada502f078c041dd78adff1c74854
Matvey Aksenov <matvey.aksenov@gmail.com>
2012-05-02 18:56:09 +0400

    XMonad: Fix tabs.

--- extended/xmonad.hs
+++ extended/xmonad.hs
@@ -14,39 +14,39 @@ import Workspaces
 import qualified Profile         as P
 
 main = do
-	xmproc <- spawnPipe "xmobar"
-	xmonad $ defaultConfig
-		-- Terminal
-		{ terminal           = myTerminal
-		-- Hooks
-		, manageHook         = myManageHook
-		, layoutHook         = myLayoutHook
-		, logHook            = myLogHook xmproc
-		, startupHook        = myStartupHook
-		-- Key bindings
-		, keys               = myKeys
-		, mouseBindings      = myMouseBindings
-		-- Other stuff
-		, focusFollowsMouse  = myFocusFollowsMouse
-		, modMask            = myModMask
-		, workspaces         = myWorkspaces
-		, borderWidth        = myBorderWidth
-		, normalBorderColor  = myNormalBorderColor
-		, focusedBorderColor = myFocusedBorderColor
-		}
+  xmproc <- spawnPipe "xmobar"
+  xmonad $ defaultConfig
+    -- Terminal
+    { terminal           = myTerminal
+    -- Hooks
+    , manageHook         = myManageHook
+    , layoutHook         = myLayoutHook
+    , logHook            = myLogHook xmproc
+    , startupHook        = myStartupHook
+    -- Key bindings
+    , keys               = myKeys
+    , mouseBindings      = myMouseBindings
+    -- Other stuff
+    , focusFollowsMouse  = myFocusFollowsMouse
+    , modMask            = myModMask
+    , workspaces         = myWorkspaces
+    , borderWidth        = myBorderWidth
+    , normalBorderColor  = myNormalBorderColor
+    , focusedBorderColor = myFocusedBorderColor
+    }
 
 -- Log hook
 myLogHook xmproc_handle = do
   updatePointer (Relative 0.5 0.5)
   dynamicLogWithPP $ xmobarPP
-	{ ppOutput = hPutStrLn xmproc_handle
-	, ppLayout = const ""
-	, ppTitle = xmobarColor P.yellowColor ""
-	, ppCurrent = xmobarColor P.yellowColor "" . const "λ"
-	, ppHidden = xmobarColor P.orangeColor ""
-	, ppHiddenNoWindows = xmobarColor P.grayLightColor ""
-	, ppSep = "<fc=" ++ P.whiteColor ++ "> | </fc>"
+    { ppOutput = hPutStrLn xmproc_handle
+    , ppLayout = const ""
+    , ppTitle = xmobarColor P.yellowColor ""
+    , ppCurrent = xmobarColor P.yellowColor "" . const "λ"
+    , ppHidden = xmobarColor P.orangeColor ""
+    , ppHiddenNoWindows = xmobarColor P.grayLightColor ""
+    , ppSep = "<fc=" ++ P.whiteColor ++ "> | </fc>"
     , ppSort = (. namedScratchpadFilterOutWorkspace) <$> ppSort defaultPP
-	}
+    }
 --
 
