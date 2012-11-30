043e44f27a2e01ab666204fb02f2658488c8aae1
Matvey Aksenov <matvey.aksenov@gmail.com>
2012-12-01 01:49:54 +0400

    Biegunka: Add coq stuff.

--- manage/Main.hs
+++ manage/Main.hs
@@ -118,6 +118,9 @@ vim = profile "vim" $ do
   "git@github.com:Shougo/neocomplcache"         --> ".vim/bundle/neocomplcache"
   "git@github.com:Lokaltog/vim-easymotion"      --> ".vim/bundle/easymotion"
 
+  "git@github.com:vim-scripts/coq-syntax"       --> ".vim/bundle/coq-syntax"
+  "git@github.com:vim-scripts/Coq-indent"       --> ".vim/bundle/coq-indent"
+
   "git@github.com:vim-scripts/YankRing.vim"     --> ".vim/bundle/YankRing"
   "git@github.com:Shougo/unite.vim"             --> ".vim/bundle/unite"
   "git@github.com:spolu/dwm.vim"                --> ".vim/bundle/dwm"
