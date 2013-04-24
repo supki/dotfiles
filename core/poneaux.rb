ed58b27d5f77001ab04ca71605f453349f016cc4
Matvey Aksenov <matvey.aksenov@gmail.com>
2013-04-25 00:33:35 +0400

    Poneaux: map ; to :.

--- core/poneaux.rb
+++ core/poneaux.rb
@@ -3,5 +3,6 @@ PONEAUX[:normalfg] = 173
 PONEAUX[:errorbg]  = 9
 PONEAUX[:keybindings] = {
   ?K => ->(o) { o.execute :move, :up, 5 },
-  ?J => ->(o) { o.execute :move, :down, 5 }
+  ?J => ->(o) { o.execute :move, :down, 5 },
+  ?; => ->(o) { o.keybindings[o.focus][?:].call(o) }
 }
