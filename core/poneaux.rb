3fc9769900632e7685943e527e24417c4551058b
Matvey Aksenov <matvey.aksenov@gmail.com>
2013-04-29 21:25:56 +0400

    Poneaux: latest changes in HEAD.

--- core/poneaux.rb
+++ core/poneaux.rb
@@ -1,8 +1,9 @@
 PONEAUX[:download] = "#{ENV['HOME']}/.torrents/files"
 PONEAUX[:normalfg] = 173
-PONEAUX[:errorbg]  = 9
+PONEAUX[:errorbg] = 9
 PONEAUX[:keybindings] = {
   ?K => ->(o) { o.execute :move, :up, 5 },
   ?J => ->(o) { o.execute :move, :down, 5 },
   ?; => ->(o) { o.keybindings[o.focus][?:].call(o) }
 }
+PONEAUX[:alignment] = :right
