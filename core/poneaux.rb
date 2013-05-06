46d97e60ccd9e3c63297e77d1ff54f03f126d23a
Matvey Aksenov <matvey.aksenov@gmail.com>
2013-05-06 23:03:45 +0400

    Poneaux: renamings.

--- core/poneaux.rb
+++ core/poneaux.rb
@@ -1,6 +1,6 @@
 PONEAUX[:download] = "#{ENV['HOME']}/.torrents/files"
-PONEAUX[:normalfg] = 173
-PONEAUX[:errorbg] = 9
+PONEAUX[:normal_fg] = 173
+PONEAUX[:error_bg] = 9
 PONEAUX[:keybindings] = {
   ?K => ->(o) { o.execute :move, :up, 5 },
   ?J => ->(o) { o.execute :move, :down, 5 },
