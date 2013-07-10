f6052c6a70a3b96a7e03f567ecb7afc390723247
Matvey Aksenov <matvey.aksenov@gmail.com>
2013-07-11 00:11:51 +0400

    Biegunka: cosmetics.

--- biegunka/Main.hs
+++ biegunka/Main.hs
@@ -49,56 +49,56 @@ dotfiles, tools, vim, emacs, misc, experimental, edwardk :: Script Sources ()
 dotfiles = profile "dotfiles" $
   git "git@github.com:supki/.dotfiles" "git/dotfiles" $ do
     unzipWithM_ link $
-      [ ("xsession", ".xsession")
-      , ("mpdconf", ".mpdconf")
-      , ("profile", ".profile")
-      , ("bashrc", ".bashrc")
-      , ("zshenv", ".zshenv")
-      , ("zshrc", ".zshrc")
-      , ("inputrc", ".inputrc")
-      , ("vimrc", ".vimrc")
-      , ("vim.custom", ".vim/plugin/vimrc-local.vim")
-      , ("ghci", ".ghci")
-      , ("irbrc", ".irbrc")
-      , ("haskeline", ".haskeline")
-      , ("racketrc", ".racketrc")
-      , ("gitconfig", ".gitconfig")
-      , ("gitignore", ".gitignore")
-      , ("ackrc", ".ackrc")
-      , ("vim/pathogen.vim", ".vim/autoload/pathogen.vim")
-      , ("vim/cscope_maps.vim", ".vim/bundle/cscope_maps.vim")
-      , ("vim/scratch", ".vim/bundle/scratch")
-      , ("vim/indent/haskell.vim", ".vim/indent/haskell.vim")
-      , ("conceal/haskell.vim", ".vim/after/syntax/haskell.vim")
-      , ("XCompose", ".XCompose")
-      , ("vimusrc", ".vimusrc")
-      , ("tmux.conf", ".tmux.conf")
-      , ("emacs", ".emacs")
-      , ("poneaux.rb", ".poneaux.rb")
-      , ("sqliterc", ".sqliterc")
+      [ "xsession" ~> ".xsession"
+      , "mpdconf" ~> ".mpdconf"
+      , "profile" ~> ".profile"
+      , "bashrc" ~> ".bashrc"
+      , "zshenv" ~> ".zshenv"
+      , "zshrc" ~> ".zshrc"
+      , "inputrc" ~> ".inputrc"
+      , "vimrc" ~> ".vimrc"
+      , "vim.custom" ~> ".vim/plugin/vimrc-local.vim"
+      , "ghci" ~> ".ghci"
+      , "irbrc" ~> ".irbrc"
+      , "haskeline" ~> ".haskeline"
+      , "racketrc" ~> ".racketrc"
+      , "gitconfig" ~> ".gitconfig"
+      , "gitignore" ~> ".gitignore"
+      , "ackrc" ~> ".ackrc"
+      , "vim/pathogen.vim" ~> ".vim/autoload/pathogen.vim"
+      , "vim/cscope_maps.vim" ~> ".vim/bundle/cscope_maps.vim"
+      , "vim/scratch" ~> ".vim/bundle/scratch"
+      , "vim/indent/haskell.vim" ~> ".vim/indent/haskell.vim"
+      , "conceal/haskell.vim" ~> ".vim/after/syntax/haskell.vim"
+      , "XCompose" ~> ".XCompose"
+      , "vimusrc" ~> ".vimusrc"
+      , "tmux.conf" ~> ".tmux.conf"
+      , "emacs" ~> ".emacs"
+      , "poneaux.rb" ~> ".poneaux.rb"
+      , "sqliterc" ~> ".sqliterc"
       ] & mapped . _1 <\>~ "core"
     unzipWithM_ link $
-      [ ("xmonad.hs", ".xmonad/xmonad.hs")
-      , ("xmonad/Controls.hs", ".xmonad/lib/Controls.hs")
-      , ("xmonad/Layouts.hs", ".xmonad/lib/Layouts.hs")
-      , ("xmonad/Startup.hs", ".xmonad/lib/Startup.hs")
-      , ("xmonad/Themes.hs", ".xmonad/lib/Themes.hs")
-      , ("xmonad/Tmux.hs", ".xmonad/lib/Tmux.hs")
-      , ("xmonad/Man.hs", ".xmonad/lib/Man.hs")
-      , ("xmonad/Workspaces.hs", ".xmonad/lib/Workspaces.hs")
-      , ("gvimrc", ".gvimrc")
-      , ("vimcolors", ".vim/colors")
-      , ("pentadactylrc", ".pentadactylrc")
-      , ("pentadactyl/wanker.penta", ".pentadactyl/plugins/wanker.penta")
-      , ("gtkrc.mine", ".gtkrc.mine")
-      , ("mplayer-config", ".mplayer/config")
+      [ "xmonad.hs" ~> ".xmonad/xmonad.hs"
+      , "xmonad/Controls.hs" ~> ".xmonad/lib/Controls.hs"
+      , "xmonad/Layouts.hs" ~> ".xmonad/lib/Layouts.hs"
+      , "xmonad/Startup.hs" ~> ".xmonad/lib/Startup.hs"
+      , "xmonad/Themes.hs" ~> ".xmonad/lib/Themes.hs"
+      , "xmonad/Tmux.hs" ~> ".xmonad/lib/Tmux.hs"
+      , "xmonad/Man.hs" ~> ".xmonad/lib/Man.hs"
+      , "xmonad/Workspaces.hs" ~> ".xmonad/lib/Workspaces.hs"
+      , "gvimrc" ~> ".gvimrc"
+      , "vimcolors" ~> ".vim/colors"
+      , "pentadactylrc" ~> ".pentadactylrc"
+      , "pentadactyl/wanker.penta" ~> ".pentadactyl/plugins/wanker.penta"
+      , "gtkrc.mine" ~> ".gtkrc.mine"
+      , "mplayer-config" ~> ".mplayer/config"
       ] & mapped . _1 <\>~ "extended"
     unzipWithM_ substitute $
-      [ ("template.xmobar.hs", ".xmobar/xmobar.hs")
-      , ("xmonad/Misc.hs.template", ".xmonad/lib/Misc.hs")
-      , ("xmonad/Profile.hs.template", ".xmonad/lib/Profile.hs")
-      , ("xmodmap.template", ".xmodmap")
-      , ("Xdefaults.template", ".Xdefaults")
+      [ "template.xmobar.hs" ~> ".xmobar/xmobar.hs"
+      , "xmonad/Misc.hs.template" ~> ".xmonad/lib/Misc.hs"
+      , "xmonad/Profile.hs.template" ~> ".xmonad/lib/Profile.hs"
+      , "xmodmap.template" ~> ".xmodmap"
+      , "Xdefaults.template" ~> ".Xdefaults"
       ] & mapped . _1 <\>~ "extended"
     shell "xrdb -merge ~/.Xdefaults"
 
@@ -106,25 +106,25 @@ dotfiles = profile "dotfiles" $
 tools = profile "tools" $
   git "git@budueba.com:tools" "git/tools" $ do
     unzipWithM_ link
-      [ ("youtube-in-mplayer.sh", "bin/youtube-in-mplayer")
-      , ("cue2tracks.sh", "bin/cue2tracks")
-      , ("weather.rb", "bin/ask-weather")
-      , ("playcount.hs", "bin/playcount")
-      , ("mpd/.lastfm.conf", ".lastfm.conf")
-      , ("mpd/lastfm.png", ".icons/lastfm.png")
-      , ("mpd/love.hs", "bin/lastfm-love-current-mpd-track")
-      , ("battery.rb", "bin/vaio-battery")
-      , ("pemised.rb", "bin/pemised")
-      , ("upload/screenshot.sh", "bin/upload-screenshot")
-      , ("upload/budueba.sh", "bin/upload-budueba")
-      , ("upload/pastebin.hs", "bin/upload-pastebin")
-      , ("isup.sh", "bin/isup")
-      , ("pretty-json.py", "bin/pretty-json")
-      , ("publish-haddocks.sh", "bin/publish-haddocks")
+      [ "youtube-in-mplayer.sh" ~> "bin/youtube-in-mplayer"
+      , "cue2tracks.sh" ~> "bin/cue2tracks"
+      , "weather.rb" ~> "bin/ask-weather"
+      , "playcount.hs" ~> "bin/playcount"
+      , "mpd/.lastfm.conf" ~> ".lastfm.conf"
+      , "mpd/lastfm.png" ~> ".icons/lastfm.png"
+      , "mpd/love.hs" ~> "bin/lastfm-love-current-mpd-track"
+      , "battery.rb" ~> "bin/vaio-battery"
+      , "pemised.rb" ~> "bin/pemised"
+      , "upload/screenshot.sh" ~> "bin/upload-screenshot"
+      , "upload/budueba.sh" ~> "bin/upload-budueba"
+      , "upload/pastebin.hs" ~> "bin/upload-pastebin"
+      , "isup.sh" ~> "bin/isup"
+      , "pretty-json.py" ~> "bin/pretty-json"
+      , "publish-haddocks.sh" ~> "bin/publish-haddocks"
       ]
     unzipWithM_ (\s d -> raw "ghc" ["-O2 ", s, "-fforce-recomp", "-v0", "-o", d] >> link d ("bin" </> d))
-      [ ("audio.hs", "vaio-audio")
-      , ("shutdown-gui.hs", "shutdown-gui")
+      [ "audio.hs" ~> "vaio-audio"
+      , "shutdown-gui.hs" ~> "shutdown-gui"
       ]
 
 
@@ -208,6 +208,10 @@ edwardk = profile "edwardk" $ mapM_ (--> "git/")
 infix 8 -->
 (-->) :: String -> FilePath -> Script Sources ()
 (-->) = git_
+
+infix 4 ~>
+(~>) :: a -> b -> (a, b)
+(~>) = (,)
 
 infixr 4 <\>~
 (<\>~) :: Setting (->) s t FilePath FilePath -> FilePath -> s -> t
