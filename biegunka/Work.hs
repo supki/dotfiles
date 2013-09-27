43ec33583447b23a24ad58182f1ee6049e044a7d
Matvey Aksenov <matvey.aksenov@gmail.com>
2013-09-27 16:29:12 +0400

    Biegunka: remove obsolete autostart commands

--- biegunka/Work.hs
+++ biegunka/Work.hs
@@ -29,8 +29,6 @@ templates = def
         , ("/usr/lib/firefox/firefox", "firefox")
         , ("thunderbird", "thunderbird")
         , ("gnome-commander", "gnome-commander")
-        , ("ssh .* mcabber", "urxvt -title mcabber -e ssh matt@budueba.com -t 'export LANG=en_US.UTF-8; TERM=rxvt tmux attach -t mcabber'")
-        , ("ssh .* irssi", "urxvt -title irssi -e ssh matt@budueba.com -t 'export LANG=en_US.UTF-8; TERM=rxvt tmux attach -t irssi'")
         , ("watch -n2 netstat", "urxvt -title netstat -e watch -n2 netstat -anptu | egrep '^Proto|:80' | sort")
         , ("htop", "urxvt -title work-htop -e htop")
         , ("htop", "urxvt -title dev-htop -e ssh dev -t htop")
