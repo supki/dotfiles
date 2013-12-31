a3a68bfa0df8f112decf8f63507c7546dc2141c3
Matvey Aksenov <matvey.aksenov@gmail.com>
2013-12-31 19:40:36 +0400

    Pakej: API changes

--- core/pakej.hs
+++ core/pakej.hs
@@ -7,19 +7,20 @@ import System.Command.QQ (sh)
 
 main :: IO ()
 main = pakej
-  [ "cpu"     ~> [sh| cpu.pl /proc/stat |]
-  , "mem"     ~> [sh| mem.awk /proc/meminfo |]
-  , "battery" ~> [sh| bat.rb |]
-  , "loadavg" ~> [sh| loadavg.awk /proc/loadavg |]
-  , delayed (60 * defaultTimeout) $
+  [ run $ "cpu"     ~> [sh| cpu.pl /proc/stat |]
+  , run $ "mem"     ~> [sh| mem.awk /proc/meminfo |]
+  , run $ "battery" ~> [sh| bat.rb |]
+  , run $ "loadavg" ~> [sh| loadavg.awk /proc/loadavg |]
+  , run . delay (60 *) $
       "ip" ~> [sh| ip.awk eth0 |]
-  , delayed (30 * defaultTimeout) $
+  , run . delay (30 *) $
       "date" ~> [sh| date.sh |]
-  , delayed (60 * defaultTimeout) $
+  , run . delay (60 *) $
       "playcount" ~> [sh| playcount |]
-  , delayed (60 * defaultTimeout) $
+  , run . delay (60 *) $
       "loadavg2" ~> [sh| pakej --hostname budueba.com --port 1234 loadavg |]
-  , delayed (60 * defaultTimeout) $
+  , run . delay (60 *) $
       "weather" ~> [sh| weather.rb |]
-  , "all" |> ["cpu", "mem", "ip", "battery", "loadavg", "loadavg2", "weather", "playcount", "date"]
+  , run $
+      "all" |> ["cpu", "mem", "ip", "battery", "loadavg", "loadavg2", "weather", "playcount", "date"]
   ]
