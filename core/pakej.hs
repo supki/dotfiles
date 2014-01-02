6224ec7413db181ae92424325fdb535bf451fff3
Matvey Aksenov <matvey.aksenov@gmail.com>
2014-01-02 18:25:51 +0400

    Pakej: cosmetics

--- core/pakej.hs
+++ core/pakej.hs
@@ -7,20 +7,18 @@ import System.Command.QQ (sh)
 
 main :: IO ()
 main = pakej
-  [ run $ "cpu"     ~> [sh| cpu.pl /proc/stat |]
-  , run $ "mem"     ~> [sh| mem.awk /proc/meminfo |]
-  , run $ "battery" ~> [sh| bat.rb |]
-  , run $ "loadavg" ~> [sh| loadavg.awk /proc/loadavg |]
-  , run . delay (60 *) $
-      "ip" ~> [sh| ip.awk eth0 |]
-  , run . delay (30 *) $
-      "date" ~> [sh| date.sh |]
-  , run . delay (60 *) $
-      "playcount" ~> [sh| playcount |]
-  , run . delay (60 *) $
-      "loadavg2" ~> [sh| pakej --hostname budueba.com --port 1234 loadavg |]
-  , run . delay (60 *) $
-      "weather" ~> [sh| weather.rb |]
+  [ every  5 $ "cpu"       ~> [sh| cpu.pl /proc/stat |]
+  , every 10 $ "mem"       ~> [sh| mem.awk /proc/meminfo |]
+  , every 30 $ "battery"   ~> [sh| bat.rb |]
+  , every 10 $ "loadavg"   ~> [sh| loadavg.awk /proc/loadavg |]
+  , every 60 $ "ip"        ~> [sh| ip.awk eth0 |]
+  , every 30 $ "date"      ~> [sh| date.sh |]
+  , every 60 $ "playcount" ~> [sh| playcount |]
+  , every 60 $ "loadavg2"  ~> [sh| pakej --hostname budueba.com --port 1234 loadavg |]
+  , every 60 $ "weather"   ~> [sh| weather.rb |]
   , run $
       "all" |> ["cpu", "mem", "ip", "battery", "loadavg", "loadavg2", "weather", "playcount", "date"]
   ]
+
+every :: Int -> Pakejee IO a -> Pakej a
+every n = run . delay (n *)
