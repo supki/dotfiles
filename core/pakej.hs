a41812228a428d6fe2a2cc6bdaf330a7011decbc
Matvey Aksenov <matvey.aksenov@gmail.com>
2014-02-21 18:20:42 +0400

    Pakej: rewrite memory widget in Haskell

--- core/pakej.hs
+++ core/pakej.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ExtendedDefaultRules #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
 module Main (main) where
@@ -20,7 +21,7 @@ import Text.Printf (printf)
 main :: IO ()
 main = pakej $ private "all" . aggregate
   [ private "cpu"       . cpu "/proc/stat"
-  , private "mem"       . system [sh| mem.awk /proc/meminfo |] . every (10 * second)
+  , private "mem"       . mem "/proc/meminfo"
   , private "ip"        . system [sh| ip.awk eth0 |] . every minute
   , private "battery"   . system [sh| bat.rb |] . every (minute `div` 2)
   , private "loadavg"   . loadavg "/proc/loadavg" . every (10 * second)
@@ -33,15 +34,21 @@ main = pakej $ private "all" . aggregate
 cpu :: FilePath -> PakejWidget Text
 cpu path = fmap format compute . constant (cpuData path)
  where cpuData = fmap (drop 1 . map read . words) . readLine
-
        compute = mkState (repeat 0) $ \_dt (v, s) ->
          let
            d = zipWith (-) v s
          in case sum d of
              t | abs t < 0.001 -> (Left undefined, s)
                | otherwise     -> (Right (sum (map (/ t) (take 3 d)) * 100 :: Double), v)
-
        format  = fromString . printf "%2.f%%"
+
+mem :: FilePath -> PakejWidget Text
+mem path = text $ do
+  t : xs <- memData path
+  return (format (usage t xs))
+ where memData    = fmap (map (read . (!! 1) . words) . take 4 . lines) . readFile
+       usage t xs = 100.0 * (t - sum xs) / t
+       format     = fromString . printf "%2.0f%%"
 
 loadavg :: FilePath -> PakejWidget Text
 loadavg path = text $ fromString . intercalate " → " . take 3 . words <$> readFile path
