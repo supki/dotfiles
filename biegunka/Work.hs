67327f876647838989484cb94e5869cbb908a7a0
Matvey Aksenov <matvey.aksenov@gmail.com>
2014-05-22 10:17:46 +0400

    Biegunka: drop thunderbird

--- biegunka/Work.hs
+++ biegunka/Work.hs
@@ -27,7 +27,6 @@ templates = def
     , startup   =
       [multiline|
         [ ("/usr/lib/firefox/firefox", "firefox")
-        , ("thunderbird", "thunderbird")
         , ("gnome-commander", "gnome-commander")
         , ("watch -n2 netstat", "urxvt -title netstat -e watch -n2 netstat -anptu | egrep '^Proto|:80' | sort")
         , ("htop", "urxvt -title work-htop -e htop")
