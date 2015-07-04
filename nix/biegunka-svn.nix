fbfb46e2abf2dad29b0b3eb460d8197edcdf5b97
Matvey Aksenov <matvey.aksenov@gmail.com>
2015-07-04 15:56:24 +0300

    Nix: update expressions

--- nix/biegunka-svn.nix
+++ nix/biegunka-svn.nix
@@ -1,15 +1,22 @@
-{ mkDerivation, base, biegunka, directory, fetchgit, process
-, stdenv, transformers
+{ mkDerivation, base, biegunka, directory, fetchgit, filepath
+, hspec, hspec-expectations-lens, lens, process, stdenv, temporary
+, transformers
 }:
 mkDerivation {
   pname = "biegunka-svn";
   version = "0.1.0";
   src = fetchgit {
     url = "https://github.com/biegunka/biegunka-svn";
-    sha256 = "6b54cdf68e2cf14e04b622e0734ba53c32e4a25cc1ef9f171a6551a32363dc02";
-    rev = "e3287f639f057c89047afd56a0fc8e350115f240";
+    sha256 = "602de4a07843ae9f30a5814c07baa0929a5d150b8786474069b2dc7eab37a1c7";
+    rev = "cd9c1d46dac750a506a3ca66ab07f20087ef8b75";
   };
-  buildDepends = [ base biegunka directory process transformers ];
+  buildDepends = [
+    base biegunka directory lens process transformers
+  ];
+  testDepends = [
+    base directory filepath hspec hspec-expectations-lens lens process
+    temporary transformers
+  ];
   homepage = "https://github.com/biegunka/biegunka-svn";
   description = "SVN support for Biegunka";
   license = stdenv.lib.licenses.bsd2;
