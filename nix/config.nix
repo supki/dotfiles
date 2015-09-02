2aeb762e69236bc3bf04079e4c4b7308fb507b5a
Matvey Aksenov <matvey.aksenov@gmail.com>
2015-09-02 13:06:14 +0300

    Nix: GHC 7.10.2

--- nix/config.nix
+++ nix/config.nix
@@ -24,6 +24,19 @@
             pakej = self.callPackage ./pakej.nix {};
           };
         };
+
+        ghc7102 = unusable.haskell.packages.ghc7102.override {
+          overrides = self: _: {
+            hdevtools = self.callPackage ./hdevtools-7.10.nix {};
+            biegunka = self.callPackage ./biegunka.nix {
+			  mkDerivation = args: self.mkDerivation(args // {
+			    buildTools = (if args ? buildTools then args.buildTools else []) ++ [ unusable.git ];
+			  });
+			};
+            biegunka-svn = self.callPackage ./biegunka-svn.nix {};
+            pakej = self.callPackage ./pakej.nix {};
+          };
+        };
       };
     };
   };
