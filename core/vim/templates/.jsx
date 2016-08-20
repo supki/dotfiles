28f21b4274ea283436fe79f0842430d79cafa0fa
Matvey Aksenov <matvey.aksenov@gmail.com>
2016-08-20 11:41:36 +0300

    Vim: update JSX template.

--- core/vim/templates/.jsx
+++ core/vim/templates/.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 
 
+const {} = React.PropTypes;
+
 export default React.createClass({
   mixins: [PureRenderMixin],
+
+  propTypes: {
+  },
 
   componentDidMount() {
   },
