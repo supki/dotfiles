8e016ce22121994f94630f954e31de3fe52082e8
Matvey Aksenov <matvey.aksenov@gmail.com>
2016-10-04 16:54:20 +0300

    2016: Cleanup.

--- core/poneaux.rb
+++
@@ -1,9 +0,0 @@
-PONEAUX[:download] = "~/.torrents/files"
-PONEAUX[:normal_fg] = 173
-PONEAUX[:error_bg] = 9
-PONEAUX[:keybindings] = {
-  ?K => ->(o) { o.execute :move, :up, 5 },
-  ?J => ->(o) { o.execute :move, :down, 5 },
-  ?; => ->(o) { o.keybindings[o.focus][?:].call(o) }
-}
-PONEAUX[:alignment] = :right
