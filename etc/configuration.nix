f6176ebade01f0772ea2421b211b79ffd6012574
Matvey Aksenov <matvey.aksenov@gmail.com>
2026-03-25 11:10:12 +0000

    nixos: update

--- etc/configuration.nix
+++ etc/configuration.nix
@@ -40,8 +40,8 @@
 
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
 
   # Configure keymap in X11
